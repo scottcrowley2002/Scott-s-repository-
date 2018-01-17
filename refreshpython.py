93377fd8018cc830aabb2e78fffc83276bbb6cbe
scottcrowley2002 <15scrowley@stjosephsrush.ie>
2018-01-17 10:29:59 +0000

    createrefreshpython.py
    going back over python

---
+++ refreshpython.py
@@ -0,0 +1,49 @@
+#welcome to mr.murrays check list
+#1.
+print("hello")
+hello#this says hello
+
+#2.sleep
+from time import sleep
+#this allows sleep to be used
+
+#3. input
+answer=input("do you like football")
+#this asks do you like football and allows youn to type an answer
+
+#4.While loop
+While True:
+#this allows something to be repeated until stopped
+
+#5. if else
+if answer=="no":
+  print("ok")
+#this prints ok if you type no
+
+elif answer=="yes":
+  print("cool")
+# this prints out cool if you type yes
+
+else:
+  print("i dont care")
+#this will print i dont care if you type anything else
+
+#6.
+def functionName ():
+#write long code here
+#this basically saves a long code into one line
+#to use this code again you have to use:
+functionName ()
+
+#7.lists
+list = ["pizza" , "chips" , " coke"]
+
+#8. For loop
+for item in list:
+  print(item)
+"""
+output looks like===
+pizza
+chips
+coke
+"""
